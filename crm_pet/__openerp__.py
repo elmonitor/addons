dee93046e7007ac969c7355454fe93d4834be54a
elmonitor <info@elmonitor.net>
2016-06-07 12:08:52 -0500

    modules added

---
+++ crm_pet/__openerp__.py
@@ -0,0 +1,19 @@
+# noinspection PyStatementEffect
+{
+    'name': 'Valoración de dieta PNF',
+    'version': '1.0',
+    'category': 'crm',
+    'description': """
+Permite calcular la valoración del alimento para mascotas
+    """,
+    'author': '3nodus',
+    'images': [],
+    'depends': ['crm'],
+    'demo': [],
+    'test': [],
+    'data': [
+        "views/crm_view.xml",
+    ],
+    'auto_install': False,
+    'installable': True,
+}
