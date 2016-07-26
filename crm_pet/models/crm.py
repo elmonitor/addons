0db5dbac86b5ac50521f62b34bdfcc10090eafe2
elmonitor <info@elmonitor.net>
2016-07-26 19:54:19 +0000

    Update crm.py

--- crm_pet/models/crm.py
+++ crm_pet/models/crm.py
@@ -27,112 +27,7 @@ class crm_pet(models.Model):
     }
 
     @api.multi
-    def datos(self):
-        valoracion=0
-        if self.actividad_pet=='muysedentario':
-            self.write({'_actividad':-0.4})
-        if self.actividad_pet=='sedentario':
-            self.write({'_actividad':-0.2})
-        if self.actividad_pet=='normal':
-            self.write({'_actividad':0})
-        if self.actividad_pet=='activo':
-            self.write({'_actividad':0.2})
-        if self.actividad_pet=='muyactivo':
-            self.write({'_actividad':0.4})
-        
-        if self.sexo_pet=="macho":
-            self.write({'_sexo': 0.2})
-        elif self.sexo_pet=="hembra":
-            self.write({'_sexo': 0.1})
-        
-        if self.edad_pet=='puppy':
-            self.write({'_raciones':4})
-            if self.condicion_pet=="muydelgado":
-                self.write({'_condicion':0.4})
-            elif self.condicion_pet=="delgado":
-                self.write({'_condicion':0.2})
-            elif self.condicion_pet=="optimo":
-                self.write({'_condicion':0.0})
-            elif self.condicion_pet=="sobrepeso":
-                self.write({'_condicion':-0.2})
-            elif self.condicion_pet=="obeso":
-                self.write({'_condicion':-0.4})
-
-            if (self.peso_pet>=0.35 and self.peso_pet<=2.1):
-                self.write({'tamano_pet':"miniatura"})
-                self.write({'_tamano':9})
-            elif (self.peso_pet>2.1 and self.peso_pet<=5.2):
-                self.write({'tamano_pet':"pequeno"})
-                self.write({'_tamano':8})
-            elif (self.peso_pet>5.2 and self.peso_pet<=8.5):
-                self.write({'tamano_pet':"mediano"})
-                self.write({'_tamano':7})
-            elif (self.peso_pet>8.5 and self.peso_pet<=13):
-                self.write({'tamano_pet':"grande"})
-                self.write({'_tamano':6})
-            elif (self.peso_pet>13):
-                self.write({'tamano_pet':"muygrande"})
-                self.write({'_tamano':5})
-
-        elif self.edad_pet=='junior':
-            self.write({'_raciones':3})
-            if (self.peso_pet>=0.7 and self.peso_pet<=4.4):
-                self.write({'tamano_pet':"miniatura"})
-                self.write({'_tamano':6.5})
-            elif (self.peso_pet>4.4 and self.peso_pet<=13.5):
-                self.write({'tamano_pet':"pequeno"})
-                self.write({'_tamano':5.2})
-            elif (self.peso_pet>13.5 and self.peso_pet<=21.3):
-                self.write({'tamano_pet':"mediano"})
-                self.write({'_tamano':4.5})
-            elif (self.peso_pet>21.3 and self.peso_pet<=27.8):
-                self.write({'tamano_pet':"grande"})
-                self.write({'_tamano':3.7})
-            elif (self.peso_pet>27.8):
-                self.write({'tamano_pet':"muygrande"})
-                self.write({'_tamano':3})
-
-            if self.condicion_pet=="muydelgado":
-                self.write({'_condicion':0.4})
-            elif self.condicion_pet=="delgado":
-                self.write({'_condicion':0.2})
-            elif self.condicion_pet=="optimo":
-                self.write({'_condicion':0.0})
-            elif self.condicion_pet=="sobrepeso":
-                self.write({'_condicion':-0.2})
-            elif self.condicion_pet=="obeso":
-                self.write({'_condicion':-0.4})
-
-
-        elif self.edad_pet=='adult':
-            self.write({'_raciones':2})
-            if (self.peso_pet>=1.5 and self.peso_pet<=5):
-                self.write({'tamano_pet':"miniatura"})
-                self.write({'_tamano':4})
-            elif (self.peso_pet>5 and self.peso_pet<=15):
-                self.write({'tamano_pet':"pequeno"})
-                self.write({'_tamano':3})
-            elif (self.peso_pet>15 and self.peso_pet<=25):
-                self.write({'tamano_pet':"mediano"})
-                self.write({'_tamano':2.5})
-            elif (self.peso_pet>25 and self.peso_pet<=35):
-                self.write({'tamano_pet':"grande"})
-                self.write({'_tamano':2})
-            elif (self.peso_pet>35):
-                self.write({'tamano_pet':"muygrande"})
-                self.write({'_tamano':1.7})
-
-            if self.condicion_pet=="muydelgado":
-                self.write({'_condicion':0.25})
-            elif self.condicion_pet=="delgado":
-                self.write({'_condicion':0.1})
-            elif self.condicion_pet=="optimo":
-                self.write({'_condicion':0.0})
-            elif self.condicion_pet=="sobrepeso":
-                self.write({'_condicion':-0.1})
-            elif self.condicion_pet=="obeso":
-                self.write({'_condicion':-0.25})
-           
+    def calculo(self):
         valoracion=self.peso_pet * (self._sexo + self._tamano + self._condicion + self._actividad)*10
         if valoracion <300:
             valoracion=50 * ((valoracion + 25) // 50)
@@ -388,8 +283,117 @@ class crm_pet(models.Model):
                 self.write({'ref_bolsa':'700'})
                 self.write({'frecuencia':'quincenal'})
                 self.write({'precio':138000})
+    
+    @api.multi
+    def datos(self):
+        valoracion=0
+
+        if self.actividad_pet=='muysedentario':
+            self.write({'_actividad':-0.4})
+        if self.actividad_pet=='sedentario':
+            self.write({'_actividad':-0.2})
+        if self.actividad_pet=='normal':
+            self.write({'_actividad':0})
+        if self.actividad_pet=='activo':
+            self.write({'_actividad':0.2})
+        if self.actividad_pet=='muyactivo':
+            self.write({'_actividad':0.4})
+        
+        if self.sexo_pet=="macho":
+            self.write({'_sexo': 0.2})
+        elif self.sexo_pet=="hembra":
+            self.write({'_sexo': 0.1})
+        
+        if self.edad_pet=='puppy':
+            self.write({'_raciones':4})
+            if self.condicion_pet=="muydelgado":
+                self.write({'_condicion':0.4})
+            elif self.condicion_pet=="delgado":
+                self.write({'_condicion':0.2})
+            elif self.condicion_pet=="optimo":
+                self.write({'_condicion':0.0})
+            elif self.condicion_pet=="sobrepeso":
+                self.write({'_condicion':-0.2})
+            elif self.condicion_pet=="obeso":
+                self.write({'_condicion':-0.4})
+
+            if (self.peso_pet>=0.35 and self.peso_pet<=2.1):
+                self.write({'tamano_pet':"miniatura"})
+                self.write({'_tamano':9})
+            elif (self.peso_pet>2.1 and self.peso_pet<=5.2):
+                self.write({'tamano_pet':"pequeno"})
+                self.write({'_tamano':8})
+            elif (self.peso_pet>5.2 and self.peso_pet<=8.5):
+                self.write({'tamano_pet':"mediano"})
+                self.write({'_tamano':7})
+            elif (self.peso_pet>8.5 and self.peso_pet<=13):
+                self.write({'tamano_pet':"grande"})
+                self.write({'_tamano':6})
+            elif (self.peso_pet>13):
+                self.write({'tamano_pet':"muygrande"})
+                self.write({'_tamano':5})
+
+        elif self.edad_pet=='junior':
+            self.write({'_raciones':3})
+            if (self.peso_pet>=0.7 and self.peso_pet<=4.4):
+                self.write({'tamano_pet':"miniatura"})
+                self.write({'_tamano':6.5})
+            elif (self.peso_pet>4.4 and self.peso_pet<=13.5):
+                self.write({'tamano_pet':"pequeno"})
+                self.write({'_tamano':5.2})
+            elif (self.peso_pet>13.5 and self.peso_pet<=21.3):
+                self.write({'tamano_pet':"mediano"})
+                self.write({'_tamano':4.5})
+            elif (self.peso_pet>21.3 and self.peso_pet<=27.8):
+                self.write({'tamano_pet':"grande"})
+                self.write({'_tamano':3.7})
+            elif (self.peso_pet>27.8):
+                self.write({'tamano_pet':"muygrande"})
+                self.write({'_tamano':3})
+
+            if self.condicion_pet=="muydelgado":
+                self.write({'_condicion':0.4})
+            elif self.condicion_pet=="delgado":
+                self.write({'_condicion':0.2})
+            elif self.condicion_pet=="optimo":
+                self.write({'_condicion':0.0})
+            elif self.condicion_pet=="sobrepeso":
+                self.write({'_condicion':-0.2})
+            elif self.condicion_pet=="obeso":
+                self.write({'_condicion':-0.4})
 
 
-    
-    
-    
+        elif self.edad_pet=='adult':
+            self.write({'_raciones':2})
+            if (self.peso_pet>=1.5 and self.peso_pet<=5):
+                self.write({'tamano_pet':"miniatura"})
+                self.write({'_tamano':4})
+            elif (self.peso_pet>5 and self.peso_pet<=15):
+                self.write({'tamano_pet':"pequeno"})
+                self.write({'_tamano':3})
+            elif (self.peso_pet>15 and self.peso_pet<=25):
+                self.write({'tamano_pet':"mediano"})
+                self.write({'_tamano':2.5})
+            elif (self.peso_pet>25 and self.peso_pet<=35):
+                self.write({'tamano_pet':"grande"})
+                self.write({'_tamano':2})
+            elif (self.peso_pet>35):
+                self.write({'tamano_pet':"muygrande"})
+                self.write({'_tamano':1.7})
+
+            if self.condicion_pet=="muydelgado":
+                self.write({'_condicion':0.25})
+            elif self.condicion_pet=="delgado":
+                self.write({'_condicion':0.1})
+            elif self.condicion_pet=="optimo":
+                self.write({'_condicion':0.0})
+            elif self.condicion_pet=="sobrepeso":
+                self.write({'_condicion':-0.1})
+            elif self.condicion_pet=="obeso":
+                self.write({'_condicion':-0.25})
+
+#class crm_lead(models.Model):
+#    _inherit = "crm.lead"    
+#    def create(self, cr, uid, vals, context=None):
+#        crm_pet.datos(self)
+#        crm_pet.calculo(self)
