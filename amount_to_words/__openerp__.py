f31023234f81c3d6588a60cb0cf5991ebad0e454
elmonitor <info@elmonitor.net>
2016-08-04 22:28:03 +0000

    Update __openerp__.py

--- amount_to_words/__openerp__.py
+++ amount_to_words/__openerp__.py
@@ -1,1 +1,17 @@
-
+# -*- coding: utf-8 -*-
+{
+    'name': 'Amount to Words',
+    'version': '1.0',
+    'category': 'Accounting',
+    'summary': 'Check printing commons',
+    'description': """
+    This module converts the payment amount to words
+    """,
+    'website': 'https://www.github.com/3nodus/addons',
+    'depends' : ['account_voucher'],
+    'data': [
+        'views/account_voucher_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
