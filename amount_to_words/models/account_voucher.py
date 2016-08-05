b9c34804121fe5faf1710c84eeef1d27ecf001b3
elmonitor <info@elmonitor.net>
2016-08-05 03:57:46 -0500

    Update account_voucher.py

--- amount_to_words/models/account_voucher.py
+++ amount_to_words/models/account_voucher.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import num2words
+import num2words as n2w
 from openerp import models, fields, api
 
 class account_voucher(models.Model):
     _inherit = 'account.voucher'
-    amount_2_word = fields.char("Amount in Words"),
+    amount_2_word = fields.char("Amount in Words")
     
     @api.onchange('amount')
     def amount2word(self, cr, uid, amount, currency_id, context=None):
         currency = self.pool['res.currency'].browse(cr, uid, currency_id, context=context)
-        amount_2_word = num2words(amount,lang='es')
+        amount_2_word = n2w.num2words(amount,lang='es')
