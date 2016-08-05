0ba9842f510249aa821eacaf8d54b3b5c6f0a3a2
elmonitor <info@elmonitor.net>
2016-08-05 11:35:13 -0500

    Update account_voucher.py

--- amount_to_words/models/account_voucher.py
+++ amount_to_words/models/account_voucher.py
@@ -14,4 +14,5 @@ class account_voucher(models.Model):
     def amount2word(self, cr, uid, amount, currency_id, context=None):
         currency = self.pool['res.currency'].browse(cr, uid, currency_id, context=context)
         amount_2_word = n2w.num2words(amount,lang='es')
-        echo amount_2_word
+        print("************************************")
+        print(amount_2_word)
