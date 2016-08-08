6d3fc7d41a194cc5d66c40f04c5dc7f8c2e50d68
elmonitor <info@elmonitor.net>
2016-08-08 13:36:19 -0500

    Update account_voucher.py

--- amount_to_words/models/account_voucher.py
+++ amount_to_words/models/account_voucher.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api
-from openerp.osv import fields, osv
-from num2words import num2words as n2w
+from openerp import models, api
+from num2words import num2words
 
-
-class account_voucher(models.Model):
+class AccountVoucher(models.Model):
     _inherit = 'account.voucher'
-    columns= {
-        'amount_2_word':fields.text("Amount in Words"),
-    }
-    @api.onchange
-    def amount2word(self, cr, uid, amount, currency_id, context=None):
-        #currency = self.pool['res.currency'].browse(cr, uid, currency_id, context=context)
-        self.write({'amount_2_word':n2w.num2words(amount,lang='es')})
-        print("************************************")
-        print(amount_2_word)
+    
+    
+    @api.multi
+    @api.onchange(amount)
+    def a2w(self):
+        _a2w = num2words(self.amount,lang='es')
+        self.write({'amount_in_word':_a2w})
