908efee41b5b9ffcc96a9a169c1b311bcb163cef
elmonitor <info@elmonitor.net>
2016-08-07 21:15:46 -0500

    Create account_voucher.py

---
+++ n2w/models/account_voucher.py
@@ -0,0 +1,12 @@
+from openerp import models, api
+from num2words import num2words
+
+class AccountVoucher(models.Model):
+    _inherit = 'account.voucher'
+    
+    
+    @api.multi
+    @api.onchange(amount)
+    def a2w(self):
+        self.amount_in_word = num2words(self.amount,lang='es')
+        
