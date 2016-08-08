54b686142934ebc64af1d9f4c7cf7fad36880be3
elmonitor <info@elmonitor.net>
2016-08-08 16:39:36 -0500

    Update account_voucher.py

--- amount_to_words/models/account_voucher.py
+++ amount_to_words/models/account_voucher.py
@@ -11,8 +11,8 @@ class AccountVoucher(models.Model):
     }
     
     @api.multi
-    @api.onchange
-    def a_2_w(self):
+    @api.onchange('amount')
+    def a_2_w(self,'amount'):
         _a2w = num2words(self.amount,lang='es')
         self.write({'a2w':_a2w})
         print("*********************")
