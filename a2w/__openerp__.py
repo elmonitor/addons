8ec2cb13e4c268a49cc536f442792200951ccc58
elmonitor <info@elmonitor.net>
2016-08-07 22:09:42 -0500

    Create __openerp__.py

---
+++ a2w/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    'name' : 'Amount in Words',
+    'version' : '1.0',
+    'summary': 'This Module allows to get the amount in words in the model account.voucher, for print in check report'
+    'description': """ TODO """
+    'category': 'Accounting',
+    'sequence': 10,
+    'website' : 'https://www.3nodus.com',
+    'depends' : ['account.voucher'],
+    'demo' : [],
+    'data' : [
+        #'account_voucher_view.xml',
+        #'security/account_voucher_security.xml',
+    ],
+    'test' : [
+    
+    ],
+    'auto_install': False,
+    'installable': True,
+}
+
