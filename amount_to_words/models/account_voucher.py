e3c8747c3a012f5013e7e4c6aa39688502cabd8c
elmonitor <info@elmonitor.net>
2016-08-11 20:22:40 +0000

    Update account_voucher.py

--- amount_to_words/models/account_voucher.py
+++ amount_to_words/models/account_voucher.py
@@ -16,7 +16,7 @@ class AccountVoucher(models.Model):
     def a_2_w(self):
         reslang=self.pool['res.users'].browse(self._cr,self._uid,self._uid,context=self._context).partner_id.lang
         #print(resusers)
-        raise Warning(reslang)
+        #raise Warning(reslang)
         #for u in resuser:
         #    print(u)
         #userid=resusers.search(self._cr,self._uid,[('id','=',self._uid)],context=self._context)
