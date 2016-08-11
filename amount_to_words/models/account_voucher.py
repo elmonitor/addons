18d80e6a30b9227eff73f3fbe1a36b4681f9f8a9
elmonitor <info@elmonitor.net>
2016-08-11 23:28:19 +0000

    Update account_voucher.py

--- amount_to_words/models/account_voucher.py
+++ amount_to_words/models/account_voucher.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# © 2016 3NODUS SAS (<http://3nodus.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import api, fields, models
 from openerp.osv import fields, osv
@@ -10,19 +12,8 @@ class AccountVoucher(models.Model):
     'a2w':fields.text('Cantidad en letras',compute='a_2_w'),
     }
     
-#    @api.multi
     @api.depends('amount')
-#    @api.onchange('amount')    
     def a_2_w(self):
         reslang=self.pool['res.users'].browse(self._cr,self._uid,self._uid,context=self._context).partner_id.lang
-        #print(resusers)
-        #raise Warning(reslang)
-        #for u in resuser:
-        #    print(u)
-        #userid=resusers.search(self._cr,self._uid,[('id','=',self._uid)],context=self._context)
-        
-        
         _aws = num2words(self.amount,lang=reslang)
         self.a2w = _aws.upper()
-#        self.write({'a2w':_a2w})
-#        print("*********************")
