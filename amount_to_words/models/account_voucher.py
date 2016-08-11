53c21269425e26cea06bdc6a94afdd4e00489180
elmonitor <info@elmonitor.net>
2016-08-11 19:02:36 +0000

    Update account_voucher.py

--- amount_to_words/models/account_voucher.py
+++ amount_to_words/models/account_voucher.py
@@ -14,8 +14,8 @@ class AccountVoucher(models.Model):
     @api.depends('amount')
 #    @api.onchange('amount')    
     def a_2_w(self):
-        #raise Warning(num2words(self.amount,lang='es'))
-        _aws = num2words(self.amount,lang='es')
+        raise Warning(res_lang.iso_code)
+        _aws = num2words(self.amount,lang=res_lang.iso_code)
         self.a2w = _aws.upper()
 #        self.write({'a2w':_a2w})
 #        print("*********************")
