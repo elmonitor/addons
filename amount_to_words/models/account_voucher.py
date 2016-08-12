18eeb6d97d9612b4bb9baaf71f753b14829f5bc5
elmonitor <info@elmonitor.net>
2016-08-12 16:57:22 +0000

    Update account_voucher.py

--- amount_to_words/models/account_voucher.py
+++ amount_to_words/models/account_voucher.py
@@ -8,7 +8,7 @@ from num2words import num2words
 
 class AccountVoucher(models.Model):
     _inherit = 'account.voucher'
-    a2w = fields.Text('Amount in text',compute='a_2_w'),
+    a2w = fields.Text('Amount in text',compute='a_2_w')
     
     @api.depends('amount')
     def a_2_w(self):
